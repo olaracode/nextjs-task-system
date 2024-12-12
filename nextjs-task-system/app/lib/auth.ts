6a3cc0fe44ef91acff4a12b827946f44ad7cff11
Octavio Lara <octavioasapchi@gmail.com>
2024-12-12 00:03:50 -0300

    🚧 WIP logout redirection

--- nextjs-task-system/app/lib/auth.ts
+++ nextjs-task-system/app/lib/auth.ts
@@ -3,7 +3,17 @@ import Github from "next-auth/providers/github";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "../db";
 
-export const { handlers, auth, signIn } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Github],
   adapter: DrizzleAdapter(db),
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+    async redirect({ url, baseUrl }) {
+      console.log(url, baseUrl);
+      return `${baseUrl}/dashboard`;
+    },
+  },
 });
