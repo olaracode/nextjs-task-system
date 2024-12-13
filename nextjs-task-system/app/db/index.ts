d0794be921dcee2064eedff8aa101023ca78a24f
Octavio Lara <octavioasapchi@gmail.com>
2024-12-13 02:35:10 -0300

    🎨 create config constant and .env.example

--- nextjs-task-system/app/db/index.ts
+++ nextjs-task-system/app/db/index.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import { config } from "@/lib/utils";
 import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(config.dbUrl, { schema });
--- nextjs-task-system/app/lib/utils.ts
+++ nextjs-task-system/app/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import "dotenv/config";
+
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const config = {
+  dbUrl: process.env.DATABASE_URL || "",
+  testUserId: process.env.TEST_USER_ID || "",
+};
