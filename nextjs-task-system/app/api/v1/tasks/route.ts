83b0322cd4d680020480d321d9cffd1f4a636aa6
Octavio Lara <octavioasapchi@gmail.com>
2024-12-14 20:54:27 -0300

    ✨ Add updated POST method

--- nextjs-task-system/app/api/v1/tasks/route.ts
+++ nextjs-task-system/app/api/v1/tasks/route.ts
@@ -1,15 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-import { db } from "@/db";
+import { createTask, getActiveTasks } from "@/db/queries/task";
 import { createTaskSchema } from "@/db/z-tasks";
 import { auth } from "@/lib/auth";
 import { ApiError } from "@/lib/errors";
+
 export async function GET(request: NextRequest) {
   const session = await auth();
   if (!session) return ApiError.unauthorized();
   //? Should pagination be added?
   //? Or perhaps add a archived enum to limit query sizes
-  const tasks = await db.query.tasks.findMany();
+  const tasks = await getActiveTasks();
   return NextResponse.json({ tasks }, { status: 200 });
 }
 
@@ -17,10 +18,33 @@ export async function GET(request: NextRequest) {
 // Is this going to integrate with other
 // Apps?
 export async function POST(request: NextRequest) {
+  const session = await auth();
+  if (!session || !session.user || !session.user.id) {
+    return ApiError.unauthorized();
+  }
+
   const body = await request.json();
   try {
     const parsedBody = createTaskSchema.parse(body);
+    const task = await createTask(parsedBody, session.user.id);
+    console.log(task);
+    return NextResponse.json(
+      {
+        task,
+      },
+      {
+        status: 201,
+      },
+    );
   } catch (err) {
     if (err instanceof z.ZodError) return ApiError.zodError(err);
+    if (err instanceof Error && err.message === "no-admin") {
+      return ApiError.miscError(
+        "Unauthorized",
+        "You can't perform this action",
+        401,
+      );
+    }
+    return ApiError.server();
   }
 }
