bcba1f7dc7be0bfd4330f630529188b8e940d0d2
Octavio Lara <octavioasapchi@gmail.com>
2024-12-14 20:55:16 -0300

    ✨ ADD sonner toasts and basic UI for testing

--- nextjs-task-system/app/layout.tsx
+++ nextjs-task-system/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,7 +19,10 @@ export default function RootLayout({
       {/* TODO: */}
       {/* For some reason the ThemeModeScript is causing a error on the browser */}
       <head></head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Toaster />
+        {children}
+      </body>
     </html>
   );
 }
