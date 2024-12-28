501ba827aafb8de61e3a5a33a2c96f6d0998e15a
Octavio Lara <octavioasapchi@gmail.com>
2024-12-28 13:57:39 -0300

    ✨ update environment configuration, rename project, and add new dependencies

--- nextjs-task-system/drizzle.config.ts
+++ nextjs-task-system/drizzle.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 import { config } from "@/lib/utils";
-const { DATABASE_URL, DATABASE_DIALECT } = process.env;
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./app/db/schema.ts",
-  dialect: (DATABASE_DIALECT as "postgresql" | "mysql" | "sqlite")!,
+  dialect: (config.dialect as "postgresql" | "mysql" | "sqlite")!,
   dbCredentials: {
     url: config.dbUrl!,
   },
